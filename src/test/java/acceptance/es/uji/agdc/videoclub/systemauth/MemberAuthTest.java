d520186d99598a9b61f5977413d967b33fe91f64
Alberto Gonzalez <al286290@uji.es>
2016-12-11 23:58:59 +0100

    HU10.2 finished

--- src/test/java/acceptance/es/uji/agdc/videoclub/systemauth/MemberAuthTest.java
+++ src/test/java/acceptance/es/uji/agdc/videoclub/systemauth/MemberAuthTest.java
@@ -7,6 +7,7 @@ import es.uji.agdc.videoclub.models.utils.UserFactory;
 import es.uji.agdc.videoclub.services.AuthenticationService;
 import es.uji.agdc.videoclub.services.UserService;
 import es.uji.agdc.videoclub.services.utils.Result;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,7 +17,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Alberto on 07/12/2016.
@@ -24,7 +26,7 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Main.class)
 @Transactional
-public class AdminAuthTest {
+public class MemberAuthTest {
     private static int FIRST_FIELD = 0;
 
     @Autowired
@@ -33,28 +35,28 @@ public class AdminAuthTest {
     @Autowired
     private UserService userService;
 
-    private User admin1;
+    private User member;
 
     @Before
     public void setUp() throws Exception {
-        admin1 = UserFactory.createAdmin()
+        member = UserFactory.createMember()
                 .setDni("10614397N")
-                .setName("Admin 1")
+                .setName("Paco Sánchez Díaz")
                 .setAddress("C/Falsa, 123, 1º")
                 .setPhone(693582471)
-                .setEmail("admin01@videoclub.com")
-                .setUsername("admin01")
-                .setPassword("admin4life");
+                .setEmail("pacosd@hotmail.com")
+                .setUsername("paquito69")
+                .setPassword("paquito69sdk");
 
     }
 
     @Test
     public void auth_correctAuthentication_returnsOk() throws Exception {
-        // Given that we've created an admin1
-        userService.create(admin1);
+        // Given that we've created a member
+        userService.create(member);
 
         // Then, we try to authenticate it
-        Result authResult = authService.auth(admin1.getUsername(), "admin4life");
+        Result authResult = authService.auth(member.getUsername(), "paquito69sdk");
 
         // Assert that auth returns OK
         assertTrue(authResult.isOk());
@@ -62,11 +64,11 @@ public class AdminAuthTest {
 
     @Test
     public void auth_incorrectAuthenticationWrongUsername_returnsLOGIN_FAILED_USERNAME() throws Exception {
-        // Given that we've created an admin1
-        userService.create(admin1);
+        // Given that we've created a member
+        userService.create(member);
 
         // Then, we try to authenticate it and we fail to introduce his/her username
-        Result authResult = authService.auth("admin001", "foo");
+        Result authResult = authService.auth("paquito6", "foo");
 
         // Assert that auth returns an error with message: LOGIN_FAILED and the field that fails is USERNAME
         assertTrue(authResult.isError());
@@ -77,15 +79,20 @@ public class AdminAuthTest {
 
     @Test
     public void auth_incorrectAuthenticationWrongPassword_returnsLOGIN_FAILED_PASSWORD() throws Exception {
-        // Given that we've created an admin1
-        userService.create(admin1);
+        // Given that we've created a member
+        userService.create(member);
 
         // Then, we try to authenticate it and we fail to introduce his/her password
-        Result authResult = authService.auth(admin1.getUsername(), "bar");
+        Result authResult = authService.auth(member.getUsername(), "foo");
 
         // Assert that auth returns an error with message: LOGIN_FAILED and the field that fails is PASSWORD
         assertTrue(authResult.isError());
         assertEquals("LOGIN_FAILED", authResult.getMsg());
         assertEquals("PASSWORD", authResult.getFields()[FIRST_FIELD]);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
     }
 }
