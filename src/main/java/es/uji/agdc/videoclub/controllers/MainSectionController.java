0d880c8425345c8ea41e41b984d4b17fa484dcb9
dnchia <daniel.chia.95@gmail.com>
2017-01-08 02:59:53 +0100

    Search part in progress, to test, lack of service.

--- src/main/java/es/uji/agdc/videoclub/controllers/MainSectionController.java
+++ src/main/java/es/uji/agdc/videoclub/controllers/MainSectionController.java
@@ -1,11 +1,23 @@
 package es.uji.agdc.videoclub.controllers;
 
 import es.uji.agdc.videoclub.helpers.ApplicationStateData;
+import es.uji.agdc.videoclub.helpers.Services;
+import es.uji.agdc.videoclub.models.Movie;
 import es.uji.agdc.videoclub.models.User;
+import es.uji.agdc.videoclub.services.MovieQueryTypeMultiple;
+import es.uji.agdc.videoclub.services.MovieService;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 /**
  * Created by daniel on 3/01/17.
@@ -23,11 +35,25 @@ public class MainSectionController extends Controller {
     @FXML
     private Label search_label;
     @FXML
-    private ListView searchResult_listView;
+    private ChoiceBox<String> searchBy_ChoiceBox;
+    @FXML
+    private VBox searchResult_VBox;
+
+    private MovieService movieService = Services.getMovieService();
 
 
     @FXML
     public void initialize() {
+        searchBy_ChoiceBox.getItems().add("Título");
+        searchBy_ChoiceBox.getItems().add("Título VO");
+        searchBy_ChoiceBox.getItems().add("Año");
+        searchBy_ChoiceBox.getItems().add("Actor");
+        searchBy_ChoiceBox.getItems().add("Director");
+        searchBy_ChoiceBox.getItems().add("Género");
+        searchBy_ChoiceBox.getItems().add("Por cualquiera");
+
+        searchBar_button.setDisable(true);
+
         User loggedUser = ApplicationStateData.getLoggedUser();
         int ADMIN_TAB_POSITION = 1;
 
@@ -37,8 +63,137 @@ public class MainSectionController extends Controller {
     }
 
     public void searchMovies() {
-        search_label.setText("Se ha buscado: " + searchBar_textField.getText());
+        if (!searchBy_ChoiceBox.getValue().equals("")) {
 
-        //TODO: Do search
+            switch (searchBy_ChoiceBox.getValue()) {
+                case "Título":
+                    searchByTitle();
+                    break;
+
+                case "Título VO":
+                    searchByTitleVO();
+                    break;
+
+                case "Año":
+                    searchByYear();
+                    break;
+
+                case "Actor":
+                    searchByActors();
+                    break;
+
+                case "Director":
+                    searchByDirector();
+                    break;
+
+                case "Género":
+                    searchByGenre();
+                    break;
+
+                case "Por cualquiera":
+                    searchByAllFields();
+                    break;
+            }
+
+            search_label.setText("Se ha buscado: " + searchBar_textField.getText());
+        }
+    }
+
+    public void checkChoiceBox() {
+        if (!searchBy_ChoiceBox.getValue().equals(""))
+            searchBar_button.setDisable(false);
+    }
+
+    private void searchByTitle() {
+        //TODO: Search by words, not by the full string
+        String searchedBy = searchBar_textField.getText();
+        searchResult_VBox.getChildren().clear();
+
+        Iterator<Movie> movies = movieService.findAllBy(MovieQueryTypeMultiple.TITLE, searchedBy).iterator();
+
+        while (movies.hasNext())
+            searchResult_VBox.getChildren().add(generateSearchContainer(movies.next()));
+    }
+
+    private void searchByTitleVO() {
+        //TODO: Search by words, not by the full string
+        String searchedBy = searchBar_textField.getText();
+        searchResult_VBox.getChildren().clear();
+
+        //TODO: Terminate, lack of search by TITLE_VO
+    }
+
+    private void searchByYear() {
+        //TODO: Search by words, not by the full string
+        String searchedBy = searchBar_textField.getText();
+        searchResult_VBox.getChildren().clear();
+
+        Iterator<Movie> movies = movieService.findAllBy(MovieQueryTypeMultiple.YEAR, searchedBy).iterator();
+
+        while (movies.hasNext())
+            searchResult_VBox.getChildren().add(generateSearchContainer(movies.next()));
+    }
+
+    private void searchByActors() {
+        //TODO: Search by words, not by the full string
+        String searchedBy = searchBar_textField.getText();
+        searchResult_VBox.getChildren().clear();
+
+        Iterator<Movie> movies = movieService.findAllBy(MovieQueryTypeMultiple.ACTORS, searchedBy).iterator();
+
+        while (movies.hasNext())
+            searchResult_VBox.getChildren().add(generateSearchContainer(movies.next()));
+    }
+
+    private void searchByDirector() {
+        //TODO: Search by words, not by the full string
+        String searchedBy = searchBar_textField.getText();
+        searchResult_VBox.getChildren().clear();
+
+        Iterator<Movie> movies = movieService.findAllBy(MovieQueryTypeMultiple.DIRECTORS, searchedBy).iterator();
+
+        while (movies.hasNext())
+            searchResult_VBox.getChildren().add(generateSearchContainer(movies.next()));
+    }
+
+    private void searchByGenre() {
+        //TODO: Search by words, not by the full string
+        String searchedBy = searchBar_textField.getText();
+        searchResult_VBox.getChildren().clear();
+
+        Iterator<Movie> movies = movieService.findAllBy(MovieQueryTypeMultiple.GENRES, searchedBy).iterator();
+
+        while (movies.hasNext())
+            searchResult_VBox.getChildren().add(generateSearchContainer(movies.next()));
+    }
+
+    private void searchByAllFields() {
+        //TODO: Search by words, not by the full string
+        String searchedBy = searchBar_textField.getText();
+        searchResult_VBox.getChildren().clear();
+
+        Iterator<Movie> movies = movieService.findAllBy(MovieQueryTypeMultiple.ALL, searchedBy).iterator();
+
+        while (movies.hasNext())
+            searchResult_VBox.getChildren().add(generateSearchContainer(movies.next()));
+    }
+
+    private HBox generateSearchContainer(Movie movie) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/views/app/mainSection/search_result.fxml"));
+
+        try {
+            HBox resultContainer = (HBox) loader.load();
+            resultContainer.setStyle("-fx-border-width: 1px; -fx-border-color: black");
+
+            ResultController controller = loader.getController();
+            controller.setMovie(movie);
+
+            return resultContainer;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
---
+++ src/main/java/es/uji/agdc/videoclub/controllers/ResultController.java
@@ -0,0 +1,38 @@
+package es.uji.agdc.videoclub.controllers;
+
+import es.uji.agdc.videoclub.models.Movie;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ * Created by daniel on 8/01/17.
+ */
+public class ResultController extends Controller {
+
+    @FXML
+    private Label title_searchResult;
+    @FXML
+    private Label titleVO_searchResult;
+    @FXML
+    private Label year_searchResult;
+    @FXML
+    private Label genres_searchResult;
+
+    private Movie movie;
+
+
+    @FXML
+    public void initialize() {
+        title_searchResult.setText(movie.getTitle());
+        titleVO_searchResult.setText(movie.getTitleOv());
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    @FXML
+    public void fullSection() {
+        //TODO: Link to the full section of the movie
+    }
+}
